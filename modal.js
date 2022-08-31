f6f8aef609b66b2fc5670e00d747f5749a003ca3
Nozhenko Vitalii <vitalik.nozhenko@gmail.com>
2022-08-31 20:35:22 +0300

    1

--- modal.js
+++ modal.js
@@ -1,8 +1,8 @@
 (() => {
   const list = document.querySelectorAll(".item-list");
-  function activoClass() {
+  function activеClass() {
     list.forEach((item) => item.classList.remove("active"));
     this.classList.add("active");
   }
-  list.forEach((item) => item.addEventListener("click", activoClass));
+  list.forEach((item) => item.addEventListener("click", activеClass));
 })();
